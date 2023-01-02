bb67f2b49864c0579e659bd793e62f2882b11f5b
smoyker2 <118481833+smoyker2@users.noreply.github.com>
2023-01-02 13:39:32 +0300

    feat:completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+    let startstring;
+    if (n < 0)
+    {
+        startstring = n * -1;
+    }
+    if (n > 0)
+    {
+        startstring = n;
+    }
+    let tostring1 = startstring.toString();
+    let arraystring = tostring1.split('')
+    let reversedarray = arraystring.reverse();
+    if (reversedarray < 1)
+    {
+        let finalstring = reversedarray * -1;
+        return (finalstring.join(''))
+    }
+    else{
+        return reversedarray.join('');
+    }
 }
